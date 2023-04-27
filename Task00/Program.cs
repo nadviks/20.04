cfd560f0c8a0a64dae0ab414b95d93167c062e73
Nadezhda.S <nadviks@mail.ru>
2023-04-27 18:33:30 +0300

    Task07

---
+++ Task00/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например: 4 -> 16 -3 -> 9 -7 -> 49
+Console.Write("Введите число: ");
+//int number = int.Parse(Console.ReadLine()!);
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+// double square2 = Math.Pow(number, 2);
+Console.WriteLine($"Квадрат числа {number} равен {square}");
+// Console.WriteLine($"Квадрат числа {number} равен {square2}");
